569ee281456bb8182a73935fc86b47ca95db9ded
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-12 20:51:10 -0300

    altering file extension to .jsx

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { GlobalStyles } from './styles/globalStyles';
 import { Home } from './templates/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Home />
   </React.StrictMode>,
 );
