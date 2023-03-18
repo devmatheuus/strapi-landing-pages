5891dbf53be7a5ab46ea521f560dbbda9cda682a
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-17 22:28:15 -0300

    add prop-types

--- app/src/Components/Heading/index.jsx
+++ app/src/Components/Heading/index.jsx
@@ -1,3 +1,5 @@
+import P from 'prop-types';
+
 import * as Styles from './styles';
 
 export const Heading = ({
@@ -17,4 +19,12 @@ export const Heading = ({
       {children}
     </Styles.Title>
   );
+};
+
+Heading.propTypes = {
+  children: P.node.isRequired,
+  isThemeDark: P.bool,
+  as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  size: P.oneOf(['small', 'medium', 'big', 'huge']),
+  isUppercase: P.bool,
 };
