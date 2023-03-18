ee9e800d1db4b5aef1308571e47a71db8e9ebafc
Matheus Silva <matheus_lsilva@hotmail.com>
2023-03-17 21:00:50 -0300

    altering deprecated import

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from './styles/globalStyles';
 import { theme } from './styles/theme';
 import { Home } from './templates/Home';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
